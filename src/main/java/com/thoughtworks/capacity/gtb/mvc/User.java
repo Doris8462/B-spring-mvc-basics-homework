2cf458c9f13d7dc7930d3f96a405c505fc24173b
Doris8462 <liandou123>
2020-09-09 17:56:01 +0800

    [haungyunjie] feat:add User

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/User.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capacity.gtb.mvc;
+import javax.validation.constraints.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @NotNull(message = "id must not be null")
+    private Integer id;
+    @NotNull(message = "username must not be null")
+    @Pattern(regexp = "[A-Za-z_0-9]{3,10}$")
+    private String username;
+    @NotNull(message = "password must not be null")
+    @Size(max=12,min=5)
+    private String password;
+    @Email
+    private String email;
+}
+
+
